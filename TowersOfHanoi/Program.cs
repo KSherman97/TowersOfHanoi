08def5fb021ac9497201c1cce5a9ee4f0cc39e3c
Kyle Sherman <ksherm337@gmail.com>
2017-01-30 19:38:14 -0500

    Restructured the files

---
+++ TowersOfHanoi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowersOfHanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TowerOfHanoi ToH = new TowerOfHanoi();
+
+            //Console.WriteLine("Solve 5!: " + ToH.RecursiveFactorial(5) + Environment.NewLine + "Press any key to continue...");
+            
+        }
+    }
+}
